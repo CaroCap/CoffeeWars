1c0858ec9d7de75ed4224cf683802b1362c8888b
CaroCap <carolinecap.event@gmail.com>
2021-12-09 18:07:30 +0100

    Revert "rangement dossiers"
    This reverts commit 6852461e25eec0db6ea61d861490d8b4a5b22f48.

---
+++ modal.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style/modal.css">
+</head>
+<body>
+    
+<div class="glass">
+    <div class="modalFormContainer">
+        <div class="modalFormThird">
+            <div class="modalFormSecond">
+                <div class="modalForm" >
+                    <?php
+                        include_once "./select_Question.php";
+                    ?>                    
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+
+
+
+
+
+
+
+
+
+</body>
+</html>
