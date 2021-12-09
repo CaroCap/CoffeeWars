0c2158cbd0d02cd159a5ece6e9bd5ae68b6de4e2
CaroCap <carolinecap.event@gmail.com>
2021-12-09 23:45:26 +0100

    Thermos dans le dossier principal

--- timer.php
+++ timer.php
@@ -5,8 +5,8 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
-    <script src="main.js"></script>  
-    <link rel="stylesheet" href="style.css">
+    <script src="./script/timer.js"></script>  
+    <link rel="stylesheet" href="./style/timer.css">
     <title>Document</title>
 </head>
 <body>
