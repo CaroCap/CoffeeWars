9b1ee98b7a4aaa8f56bbaf8c398fe3857103db31
CaroCap <carolinecap.event@gmail.com>
2021-12-17 17:02:12 +0100

    Remise à 180sec du chrono

--- script/main.js
+++ script/main.js
@@ -1,8 +1,8 @@
 // CONSTANTES DEPART : Lancement du jeu (+ pour permettre une remise à 0 si ajout d'un bouton rejouer)
 const scoreStart = 0;
 // Mise au max du Timer
-const timeLeftStart = 10;
-const timeTotalStart = 10;
+const timeLeftStart = 180;
+const timeTotalStart = 180;
 
 const finPartieStart = false;
 
