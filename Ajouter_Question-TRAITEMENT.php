09395c7cb976813b7255766e3928cacbd0f41f5e
CaroCap <carolinecap.event@gmail.com>
2022-02-10 12:02:01 +0100

    changement fleche et pimpage de page traitement

--- Ajouter_Question-TRAITEMENT.php
+++ Ajouter_Question-TRAITEMENT.php
@@ -16,49 +16,56 @@
 </head>
 <body>
     <?php
-        // // LIAISON BD
-        // include_once "./config/db.php";
-        // try {
-        //     $bdd = new PDO(DBDRIVER . ':host=' . DBHOST . ';port=' . DBPORT . ';dbname=' . DBNAME . ';charset='. DBCHARSET, DBUSER, DBPASS);
-        // } catch (Exception $e) {
-        //     echo $e->getMessage();
-        //     die();
-        // }   
+        // LIAISON BD
+        include_once "./config/db.php";
+        try {
+            $bdd = new PDO(DBDRIVER . ':host=' . DBHOST . ';port=' . DBPORT . ';dbname=' . DBNAME . ';charset='. DBCHARSET, DBUSER, DBPASS);
+        } catch (Exception $e) {
+            echo $e->getMessage();
+            die();
+        }   
 
-        // include "./vendor/autoload.php";
+        include "./vendor/autoload.php";
 
-        // $nouvelleQuestion = new Question(["intitule_question"=>$_POST["intitule_question"], "ID_type"=>$_POST["ID_type"]]);
-        // $managerQuestion = new QuestionManager($bdd);
-        // $managerQuestion->insert($nouvelleQuestion);
-        // // récupérer l'ID de la question
-        // $ID_question = $nouvelleQuestion->getid();
+        $nouvelleQuestion = new Question(["intitule_question"=>$_POST["intitule_question"], "ID_type"=>$_POST["ID_type"]]);
+        $managerQuestion = new QuestionManager($bdd);
+        $managerQuestion->insert($nouvelleQuestion);
+        // récupérer l'ID de la question
+        $ID_question = $nouvelleQuestion->getid();
 
-        // $nouvelleReponse = new Reponse(["intitule_reponse"=>$_POST["reponse1"], "ID_question"=>$ID_question, "resultat"=>$_POST["resultat1"]]);
-        // $managerReponse = new ReponseManager($bdd);
-        // $managerReponse->insert($nouvelleReponse);
+        $nouvelleReponse = new Reponse(["intitule_reponse"=>$_POST["reponse1"], "ID_question"=>$ID_question, "resultat"=>$_POST["resultat1"]]);
+        $managerReponse = new ReponseManager($bdd);
+        $managerReponse->insert($nouvelleReponse);
 
-        // $nouvelleReponse = new Reponse(["intitule_reponse"=>$_POST["reponse2"], "ID_question"=>$ID_question, "resultat"=>$_POST["resultat2"]]);
-        // $managerReponse = new ReponseManager($bdd);
-        // $managerReponse->insert($nouvelleReponse);
+        $nouvelleReponse = new Reponse(["intitule_reponse"=>$_POST["reponse2"], "ID_question"=>$ID_question, "resultat"=>$_POST["resultat2"]]);
+        $managerReponse = new ReponseManager($bdd);
+        $managerReponse->insert($nouvelleReponse);
 
-        // $nouvelleReponse = new Reponse(["intitule_reponse"=>$_POST["reponse3"], "ID_question"=>$ID_question, "resultat"=>$_POST["resultat3"]]);
-        // $managerReponse = new ReponseManager($bdd);
-        // $managerReponse->insert($nouvelleReponse);
+        $nouvelleReponse = new Reponse(["intitule_reponse"=>$_POST["reponse3"], "ID_question"=>$ID_question, "resultat"=>$_POST["resultat3"]]);
+        $managerReponse = new ReponseManager($bdd);
+        $managerReponse->insert($nouvelleReponse);
 
     ?>
 
     <div>
+        <div id="blocGrainCafe">
+            <a href="./index.php">
+                <img class="grain_cafe" id="grain_cafe" src="./assets/img/grain_cafe.png" alt="">
+                <p class="texteGrainCafe">Retour au Jeu</p>
+            </a>
+        </div>
+
             <div>
                 <div id="questionsBlocTraitement">
 
-                    <h1 class="bratsy titreForm">Merci</h1>
-                    <p class="dongleFont">
-                    <a href="./Ajouter_Question-FORM.php">Ajouter une autre question</a>        
+                    <h1 class="titreFormTraitrement">Merci</h1>
+                    <p class="pTraitement dongleFont">
+                    <a class="aTraitement" href="./Ajouter_Question-FORM.php">Ajouter une autre question</a>        
                     </p>
                     
                 </div>
                 <!--  Div 1: div derrière en rouge  -->
-                <div id="divRouge">
+                <div id="divRougeTraitement">
                 </div>
             </div>
         </div>
--- script/main.js
+++ script/main.js
@@ -314,12 +314,12 @@ function ouvrirModal(idModal){
 
 // MOUVEMENT FLECHE Haut/Bas VERS SPIN
 const placeFleche = document.getElementById("fleche");
-let topPosition = -45;
+let topPosition = -50;
 let direction = -1;
 
 function hautBas(){
-    if(topPosition == -45) {direction = 1}
-    else if (topPosition == -25) {direction =-1}
+    if(topPosition == -50) {direction = 1}
+    else if (topPosition == -30) {direction =-1}
     topPosition += 1* direction;
     placeFleche.style.top = `${topPosition}px`
     requestAnimationFrame(hautBas);
